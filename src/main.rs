f89c477cef2d3731670a7fd1bf18013a335f3391
yizhou7 <yizhou7@outlook.com>
2022-11-15 13:38:41 -0500

    add commandline args

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,10 @@
 use std::collections::BTreeMap;
-use std::env;
 use std::fs::File;
 use std::io::BufReader;
-use smt2parser::{CommandStream, concrete, renaming, visitors};
-
 use rand::thread_rng;
 use rand::seq::SliceRandom;
-use smt2parser::concrete::Command;
+use smt2parser::{CommandStream, concrete, renaming, visitors};
+use rustop::opts;
 
 fn print_non_info_command(commands: &Vec<concrete::Command>) {
     for command in commands {
@@ -52,7 +50,7 @@ fn get_assert_intervals(commands: &Vec<concrete::Command>) -> Vec<usize>
     intervals
 }
 
-fn shuffle_asserts(commands: &mut Vec<Command>) {
+fn shuffle_asserts(commands: &mut Vec<concrete::Command>) {
     let intervals = get_assert_intervals(&commands);
     let mut i = 0;
     while i < intervals.len()
@@ -68,7 +66,7 @@ fn shuffle_asserts(commands: &mut Vec<Command>) {
     }
 }
 
-fn normalize_commands(commands: Vec<concrete::Command>) {
+fn normalize_commands(commands: Vec<concrete::Command>) -> Vec<concrete::Command> {
     let randomization_seed = 121210;
     let randomization_space = BTreeMap::from([
         (visitors::SymbolKind::Variable, 100000),
@@ -85,18 +83,26 @@ fn normalize_commands(commands: Vec<concrete::Command>) {
         randomization_seed,
     };
     let mut normalizer = renaming::SymbolNormalizer::new(concrete::SyntaxBuilder, config);
+    let mut ncommand = Vec::<concrete::Command>::new();
     for command in commands {
         let command = command.accept(&mut normalizer).unwrap();
-        println!("{}", command);
+        ncommand.push(command)
     }
+    ncommand
 }
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
-    let file_path = &args[1];
-    let operation = &args[2];
+    let (args, rest) = opts! {
+        synopsis "mariposa is a smtlib2 query mutator";
+        opt in_file_path:String,
+            desc: "the input file path";
+        opt mutation:String=String::from("none"),
+            desc: "the mutation";
+        opt silent:bool=false,
+            desc: "process without printing";
+    }.parse_or_exit();
 
-    let file = File::open(file_path).unwrap();
+    let file = File::open(args.in_file_path).unwrap();
     let reader = BufReader::new(file);
 
     let stream = CommandStream::new(
@@ -108,13 +114,16 @@ fn main() {
     let mut commands :Vec<concrete::Command> =
         stream.collect::<Result<Vec<_>, _>>().unwrap();
 
-    if operation == "parse" {
-        print_non_info_command(&commands);
-    } else if operation == "shuffle" {
+    if args.mutation == "none" {
+        println!("{}", commands.len());
+    } else if args.mutation == "shuffle" {
         shuffle_asserts(&mut commands);
+    } else if args.mutation == "normalize" {
+        commands = normalize_commands(commands);
+    }
+
+    if !args.silent {
         print_non_info_command(&commands);
-    } else if operation == "normalize" {
-        normalize_commands(commands);
     }
 }
 
