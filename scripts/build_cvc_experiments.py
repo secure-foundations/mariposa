d0bb346e4b4288a74167570b25a080ad5b4205a5
Jessica Li <jgli@andrew.cmu.edu>
2023-07-27 15:37:25 -0400

    add timing, add nr

--- scripts/build_cvc_experiments.py
+++ scripts/build_cvc_experiments.py
@@ -4,7 +4,7 @@ from basic_utils import *
 def run_rules():
     return f"""
 rule run-query
-    command = ./solvers/cvc5-1.0.3 $in --incremental --tlimit 60000 > $out
+    command = (/usr/bin/time -o '$out' -a ./solvers/cvc5-1.0.3 --incremental $in --tlimit 60000) >> $out
 """
 
 # create rules for data/verus_cvc/deleted_partial_order
@@ -25,5 +25,15 @@ def modified_partial_order():
         f_out = f.replace(".smt2", ".out").replace("/modified_partial_order/", "/modified_partial_order_results/")
         print(f"build {f_out}: run-query {f}")
 
+# create rules for data/verus_cvc/nr_deleted_partial_order
+def nr_deleted_partial_order():
+    print(run_rules())
+    files = list_smt2_files("data/verus_cvc/nr_deleted_partial_order")
+    os.system("mkdir -p data/verus_cvc/nr_deleted_partial_order_results")
+    for f in files:
+        f_out = f.replace(".smt2", ".out").replace("/nr_deleted_partial_order/", "/nr_deleted_partial_order_results/")
+        print(f"build {f_out}: run-query {f}")
+
 # deleted_partial_order()
-modified_partial_order()
+# modified_partial_order()
+nr_deleted_partial_order()
--- scripts/verus_cleanup.py
+++ scripts/verus_cleanup.py
@@ -15,6 +15,8 @@ def clean_file(file, new_folder_path):
             # Remove any lines with partial-order (compare to adding replacement definition)
             if "partial-order" in line:
                 continue
+            if "(declare-datatypes () ())" in line:
+                continue
             new_lines.append(line)
     # Write new file
     new_file = os.path.join(new_folder_path, os.path.basename(file))
@@ -45,6 +47,8 @@ def clean_file_partial_order_fix(file, new_folder_path):
             if "partial-order" in line:
                 new_lines += replacement
                 continue
+            if "(declare-datatypes () ())" in line:
+                continue
             new_lines.append(line)
     # Write new file
     new_file = os.path.join(new_folder_path, os.path.basename(file))
@@ -64,4 +68,4 @@ if __name__ == '__main__':
         os.makedirs(new_folder_path)
     files = list_smt2_files(folder_path)
     for file in files:
-        clean_file_partial_order_fix(file, new_folder_path)
+        clean_file(file, new_folder_path)
