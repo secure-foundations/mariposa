3e4204c0f5e1c952d5addee6386d1f205f47e587
Yi Zhou <yizhou7@outlook.com>
2023-08-31 15:27:54 -0400

    update config for fs_vwasm_uc_trigger_10

--- scripts/analyze_tigger.py
+++ scripts/analyze_tigger.py
@@ -15,18 +15,20 @@ c = Configer()
 #     base = q.split("/")[-1]
 #     print(f"./target/release/mariposa --in-file-path {q} --out-file-path {prefix}/{base} -m remove-trigger")
 
-mini = c.load_known_project("d_komodo_uc")
-prefix = "data/unsat_cores/d_komodo_z3/min_asserts_no_trigger2/"
+mini = c.load_known_project("fs_vwasm_uc")
+prefix = "data/unsat_cores/fs_vwasm_z3/min_asserts_no_trigger2"
 
 # for q in mini.list_queries():
 #     base = q.split("/")[-1]
 #     print(f"./target/release/mariposa --in-file-path {q} --out-file-path {prefix}/{base} -m remove-trigger")
 
 diff = 0
+
 for q in mini.list_queries():
     base = q.split("/")[-1]
     out_file = f"{prefix}/{base}"
     # diff out_file with q
+    # print(out_file)
     o, _, _ = subprocess_run(f"diff {q} {out_file}")
     if o == "":
         os.remove(out_file)
