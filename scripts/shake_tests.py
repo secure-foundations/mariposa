d870ee680bb4821751df1e9409f09d2b98f5ecbe
Yi Zhou <yizhou7@outlook.com>
2023-10-07 22:06:54 -0400

    config for fs_vwasm_fun_assert

--- scripts/shake_tests.py
+++ scripts/shake_tests.py
@@ -1,10 +1,11 @@
 from basic_utils import *
 from configer import *
+import sys
 
 STABLE_SAMPLES = "data/benchmarks/stable_core/"
 UNSTABLE_SAMPLES = "data/test_set/"
 
-def emit_build_file():
+def emit_build_file(in_dir, out_dir):
     print("""
 rule shake
     command = ./target/release/mariposa -i $in -m fun-assert -o $out
@@ -12,14 +13,14 @@ rule shake
 rule z3
     command = ./solvers/z3-4.12.2 $in -T:10 > $out
 """)
-    
-    for query in list_files_ext(STABLE_SAMPLES, ".smt2"):
-        base = os.path.basename(query)
-        shake = f"gen/shake_satble/{base}"
-        print(f"build {shake}: shake {query}")
-        print(f"build {shake}.rst: z3 {shake}")
 
-emit_build_file()
+    for query in list_files_ext(in_dir, ".smt2"):
+        base = os.path.basename(query)
+        shake = f"{out_dir}/{base}"
+        print(f"build {shake}: shake {query}")
+        # print(f"build {shake}.rst: z3 {shake}")
+
+emit_build_file(sys.argv[1], sys.argv[2])
 
 # os.system('grep -rnw "unknown" -l  gen/shake_satble/*.rst')
 
