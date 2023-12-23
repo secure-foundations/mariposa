695439302fe348590a10603068c24ef2ccd048e1
Yi Zhou <yizhou7@outlook.com>
2023-12-23 11:42:40 -0500

    update db split script

--- scripts/utils/db_utils.py
+++ scripts/utils/db_utils.py
@@ -1,13 +1,39 @@
 import sqlite3, os
-# from basic_utils import *
 
-def zip_db():
-    os.system("cd data && rm chunk.tar.gz.*")
-    os.system("cd data && tar cvzf - mariposa.db | split --bytes=50MB - chunk.tar.gz.")
+def split_zip_db(db_path):
+    from sys_utils import san_check
+    import glob
 
-def unzip_db():
-    os.system("cd data && mv mariposa.db mariposa.temp.db")
-    os.system("cd data && cat chunk.tar.gz.* | tar xzvf -")
+    DB_ROOT = "data/databases"
+
+    san_check(db_path.startswith(DB_ROOT), 
+              f"[WARN] {db_path} not under {DB_ROOT}, skipping")
+
+    san_check(os.path.exists(db_path), 
+        f"[WARN] {db_path} does not exist, skipping")
+
+    base_name = os.path.basename(db_path)
+
+    split_pattern = f"{DB_ROOT}/{base_name}.tar.gz.*"
+    splits = glob.glob(split_pattern)
+
+    if len(splits) > 0:
+        print(f"[WARN] {base_name} split already exists, remove? [Y]", end=" ")
+        san_check(input() == "Y", f"[INFO] aborting")
+        os.system(f"rm {split_pattern}")
+
+    print(f"[INFO] splitting {db_path} into 50MB chunks")
+    os.system(f"cd {DB_ROOT} && tar cvzf - {base_name} | split --bytes=50MB - {base_name}.tar.gz.")
+
+    splits = glob.glob(split_pattern)
+    san_check(len(splits) > 0, f"[WARN] {base_name} split failed!")
+
+    print(f"[INFO] {base_name} split into {len(splits)} chunks: ")
+    print("\t" + " ".join(splits))
+
+# def merge_unzipped_db():
+    # os.system("cd data && mv mariposa.db mariposa.temp.db")
+    # os.system("cd data && cat chunk.tar.gz.* | tar xzvf -")
 
 def get_cursor(db_path):
     con = sqlite3.connect(db_path)
@@ -86,74 +112,8 @@ def get_mutant_rows(cur, exp_table_name, v_path, mutation):
         AND perturbation = ?""", (v_path, mutation))
     return res.fetchall()
 
-# def export_timeouts(cfg, solver):
-#     con, cur = get_cursor(cfg.qcfg.db_path)
-#     solver_table = cfg.qcfg.get_solver_table_name(solver)
-
-#     if not table_exists(cur, solver_table):
-#         print(f"[WARN] export timeout: {solver_table} does not exist!")
-#         con.close()
-#         return
-#     clean_dir = cfg.qcfg.project.clean_dirs[solver]
-#     assert clean_dir.endswith("/")
-#     target_dir = clean_dir[:-1] + "_"+ str(solver) + "_ext/"
-
-#     res = cur.execute(f"""
-#         SELECT vanilla_path, perturbation, command FROM {solver_table}
-#         WHERE result_code = "timeout" """)
-
-#     rows = res.fetchall()
-#     # print(len(rows))
-
-#     for row in rows:
-#         vanilla_path = row[0]
-#         perturb = row[1]
-#         assert vanilla_path.endswith(".smt2")
-#         assert vanilla_path.startswith(clean_dir)
-#         stemed = vanilla_path[len(clean_dir):-5]
-#         command = row[2]
-#         [solver_path, mut_path, limit] = command.split(" ")
-#         index = mut_path.index(stemed) + len(stemed)
-#         info = mut_path[index:].split(".")
-#         # print(vanilla_path)
-#         if perturb is None:
-#             command = f"cp {vanilla_path} {target_dir}"
-#         else:
-#             seed = int(info[1])
-#             assert perturb == info[2]
-#             file_name = f"{str(seed)}.{perturb}.smt2"
-#             mutant_path = target_dir + stemed + "." + file_name
-#             command = f"./target/release/mariposa -i {vanilla_path} -p {perturb} -o {mutant_path} -s {seed}"
-#         print(command)
-#     con.close()
-
-# def drop_query(db_path, exp_tname, sum_tname, query_path):
-#     con, cur = get_cursor(db_path)
-#     exp_exists = table_exists(cur, exp_tname)
-#     sum_exists = table_exists(cur, sum_tname)
-
-#     # check if table exists in the database
-#     san_check(exp_exists and sum_exists, f"[ERROR] table {exp_tname} or {sum_tname} does not exist")
-
-#     # check if the query is already in the table
-#     cur.execute(f"SELECT * FROM {exp_tname} WHERE vanilla_path = '{query_path}'")
-#     rows0 = cur.fetchall()
-
-#     if len(rows0) > 0:
-#         print(f"[INFO] query {query_path} already in the table, remove it? [Y]")
-#         san_check(input() == "Y", f"[INFO] aborting")
-#         cur.execute(f"DELETE FROM {exp_tname} WHERE vanilla_path = '{query_path}'")
-
-#     cur.execute(f"SELECT * FROM {sum_tname} WHERE vanilla_path = '{query_path}'")
-#     rows1 = cur.fetchall()
-
-#     if len(rows1) > 0:
-#         print(f"[INFO] query {query_path} already in the summary table, remove it? [Y]")
-#         san_check(input() == "Y", f"[INFO] aborting")
-#         cur.execute(f"DELETE FROM {sum_tname} WHERE vanilla_path = '{query_path}'")
-
-#     con.commit()
-#     con.close()
-
 if __name__ == "__main__":
-    pass
+    import sys
+
+    if sys.argv[1] == "split":
+        split_zip_db(sys.argv[2])
