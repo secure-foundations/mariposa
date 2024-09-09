c24135d6bd8efdb129c33b94308cf729e7817108
Yi Zhou <yizhou7@outlook.com>
2024-09-09 10:58:03 +0800

    miscs

--- src/qi_test.py
+++ src/qi_test.py
@@ -2,6 +2,17 @@ import sys
 from z3 import *
 set_param(proof=True)
 
+VERUS_OPTS = """(set-option :auto_config false)
+(set-option :smt.mbqi false)
+(set-option :smt.case_split 3)
+(set-option :smt.qi.eager_threshold 100.0)
+(set-option :smt.delay_units true)
+(set-option :smt.arith.solver 2)
+(set-option :smt.arith.nl false)
+(set-option :pi.enabled false)
+(set-option :rewriter.sort_disjunctions false)
+"""
+
 def format_expr(e, depth):
   if is_const(e):
     return str(e)
@@ -74,53 +85,44 @@ def get_assertion_qid(exp: z3.ExprRef):
 
 class Instantiater:
   def __init__(self, in_file_path, out_file_path):
-    s = Solver()
-    s.from_file(in_file_path)
+    self.proc_solver = Solver()
+    self.proc_solver.from_file(in_file_path)
 
+    # map qid to its assertion (incomplete)
+    self.handled_quants = dict()
+
+    for a in self.proc_solver.assertions():
+      qid = get_assertion_qid(a)
+      if qid is not None:
+        self.handled_quants[qid] = a
+
+    # map qid to its quant-inst applications
     self.insts = dict()
-    self.quants = dict()
     self.__visited = set()
 
-    res = s.check()
+    self.process()
+    self.output(out_file_path)
+
+  def process(self):
+    res = self.proc_solver.check()
     print(res)
-    p = s.proof()
+
+    p = self.proc_solver.proof()
     self.match_qis(p)
 
-    s2 = Solver()
+    inst_freq = map(lambda x: (x[0], len(x[1])),
+                    self.insts.items())
+    self.inst_freq = sorted(inst_freq, 
+                            key=lambda x: x[1], reverse=True)
 
-    for a in s.assertions():
-      qid = get_assertion_qid(a)
-
-      if qid is not None:
-        self.quants[qid] = a
-
-    ranked = sorted(self.insts.items(),
-                  key=lambda x: len(x[1]), reverse=True)
-
-    handled = set()
-    for qid, insts in ranked:
-      if qid not in self.quants:
-        print("unhandled", qid)
-        continue
-      q = self.quants[qid]
+    for qid, count in self.inst_freq:
+      # q = self.quants[qid]
       # qf = is_quantifier_free(q.body())
-      print(len(insts), qid)
-      handled |= {qid}
-      # if not qf:
-      #   for inst in insts:
-      #     print(inst)
-      for inst in insts:
-        s2.add(inst)
-
-    for a in s.assertions():
-      qid = get_assertion_qid(a)
-      if qid in handled:
-        print("handled", qid)
+      print(count, qid, end=" ")
+      if qid not in self.handled_quants:
+        print("(unhandled)")
         continue
-      s2.add(a)
-      
-    with open(out_file_path, "w") as f:
-      f.write(s2.to_smt2())
+      print("")
 
   def match_qis(self, p):
     oid = p.get_id()
@@ -135,15 +137,53 @@ class Instantiater:
       return
 
     res = match_qi(p)
+
     if res is not None:
       qid, qi = res
+      # if not is_quantifier_free(qi):
+        # print(qid)
+        # print(qi)
       self.insts[qid] = self.insts.get(qid, set()) | {qi}
       return
 
     for c in p.children():
       self.match_qis(c)
 
+  def output(self, out_file_path):
+    out_solver = Solver()
 
+    replaced = set()
+    added = 0
+
+    for qid, count in self.inst_freq:
+      if qid not in self.handled_quants:
+        continue
+      # if qid not in {"mariposa_qid_37"}:
+      #   continue
+      added += count
+      replaced |= {qid}
+
+      print(qid)
+      for inst in self.insts[qid]:
+        out_solver.add(inst)
+        print(format_expr(inst, 0))
+
+    replaced = {"mariposa_qid_33"}
+
+    for a in self.proc_solver.assertions():
+      qid = get_assertion_qid(a)
+
+      # if qid in replaced:
+      #   print("replaced", qid)
+      #   continue
+      out_solver.add(a)
+
+    print("replaced", len(replaced), "assertions")
+    print("added", added, "assertions")
+
+    with open(out_file_path, "w") as f:
+      f.write(VERUS_OPTS)
+      f.write(out_solver.to_smt2())
 
 if __name__ == "__main__":
   Instantiater(sys.argv[1], "out.smt2")
