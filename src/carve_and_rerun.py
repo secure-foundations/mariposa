317af38ecf455999983cdcb0e9874e42f118b177
Yi Zhou <yizhou7@outlook.com>
2025-02-03 15:13:43 -0500

    some high level changes

--- src/carve_and_rerun.py
+++ src/carve_and_rerun.py
@@ -1,5 +1,5 @@
 import sys, os
-from utils.system_utils import list_smt2_files, log_info
+from utils.system_utils import list_smt2_files, log_info, log_warn
 
 project_dir = sys.argv[1]
 assert project_dir.endswith(".filtered/base.z3")
@@ -17,7 +17,7 @@ for i in range(8):
     if query_count <= 10:
         break
 
-    exp_command = f"./src/exper_wizard.py manager -e filter --total-parts 10 -s z3_4_13_0 -i {project_dir} --clear"
+    exp_command = f"./src/exper_wizard.py manager -e filter --total-parts 12 -s z3_4_13_0 -i {project_dir} --clear"
     log_info(f"iteration {i}, current query count: {query_count}, experimenting...")
     os.system(exp_command)
 
@@ -28,9 +28,8 @@ for i in range(8):
 query_count = len(list_smt2_files(project_dir))
 
 if query_count == 0:
-    log_info("no queries left, exiting...")
-    sys.exit(0)
+    log_warn("no queries left!")
 
 print(f"carving done, running full stability test... on {query_count} queries")
-exp_command = f"./src/exper_wizard.py manager -e default --total-parts 10 -s z3_4_13_0 -i {project_dir} --clear"
+exp_command = f"./src/exper_wizard.py manager -e default --total-parts 12 -s z3_4_13_0 -i {project_dir} --clear"
 os.system(exp_command)
